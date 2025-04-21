0e53bba61521ce4c9f4a2c0093431f652fc3e0bd
OrewaKimi <kimi93.com@gmail.com>
2025-04-21 22:45:34 +0700

    update title and body

---
+++ lib/components/nested_scroll_view_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class NestedScrollViewWidget extends StatelessWidget {
+  const NestedScrollViewWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return NestedScrollView(
+      headerSliverBuilder: (context, innerBoxIsScrolled) => [
+        SliverAppBar(
+          floating: true,
+          snap: true,
+          backgroundColor: Colors.deepPurple[400],
+          title: const SearchView(),
+        )
+      ],
+      body: const TabHost(),
+    );
+  }
+}
